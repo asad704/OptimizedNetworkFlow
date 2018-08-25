a67c10c40d4cd180c91110b7e75490db876365ca
Muhammadusmanzafar <34206809+Muhammadusmanzafar@users.noreply.github.com>
2018-08-26 03:05:48 +0500

    update graphcode.py
    reading from file  and creating all the objects automatically

--- graphCode.py
+++ graphCode.py
@@ -1,5 +1,11 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import pandas as pd
+
+#edit your file path
+wl = pd.read_csv("C:/Users/MuhammadZafar/Downloads/WaterLevel.csv")
+mf = pd.read_csv("C:/Users/MuhammadZafar/Downloads/MainFlows.csv")
+
 
 class Reserve:
     
@@ -28,27 +34,38 @@ options = {
 
 }
 
-res1 = Reserve(1,"Tarbela Dam",1500,0,249.0,120.5,38.088,72.699)
-res2 = Reserve(2,"Kalabagh Dam",950,0,258.3,251.7,32.956,71.614)
-res3 = Reserve(3,"Chashma Dam",950,0,318.2,300.4,32.436,71.38)
-res4 = Reserve(4,"Tunsa Dam",1000,0,312.9,293.4,25.132,98.126)
-res5 = Reserve(5,"Guddu",1200,0,219.9,182.9,28.392,69.772)
+res = []
+for i in range(len(mf)):
+    for j in range(len(wl)):
+        if(mf.iloc[:,2][i] == wl.iloc[:,2][j]):
+            temp = []
+            temp =  Reserve(j+1,mf.iloc[:,2][i],mf.iloc[:,3][i],wl.iloc[:,3][j],mf.iloc[:,4][i],mf.iloc[:,5][i],mf.iloc[:,12][i],mf.iloc[:,13][i])
 
+        
+            res.append(temp)       
 
-print(res1.describe())
-print(res2.describe())
-print(res3.describe())
-print(res4.describe())
-print(res5.describe())
+    
+#res[0] = Reserve(1,"Tarbela Dam",1500,0,249.0,120.5,38.088,72.699)
+#res[1] = Reserve(2,"Kalabagh Dam",950,0,258.3,251.7,32.956,71.614)
+#res[2] = Reserve(3,"Chashma Dam",950,0,318.2,300.4,32.436,71.38)
+#res[3] = Reserve(4,"Tunsa Dam",1000,0,312.9,293.4,25.132,98.126)
+#res[4] = Reserve(5,"Guddu",1200,0,219.9,182.9,28.392,69.772)
+
+#print(test.describe())
+print(res[0].describe())
+print(res[1].describe())
+print(res[2].describe())
+print(res[3].describe())
+print(res[4].describe())
 
 
 flow=nx.DiGraph() # make a directed graph (digraph)
 
-flow.add_nodes_from([res1.name,res2.name,res3.name,res4.name,res5.name])
-flow.add_edges_from([(res1.name,res2.name),
-                     (res2.name,res3.name),
-                     (res3.name,res4.name),
-                     (res4.name,res5.name)],weight=1.0)
+flow.add_nodes_from([res[0].name,res[1].name,res[2].name,res[3].name,res[4].name])
+flow.add_edges_from([(res[0].name,res[1].name),
+                     (res[1].name,res[2].name),
+                     (res[2].name,res[3].name),
+                     (res[3].name,res[4].name)],weight=1.0)
 
 
 
