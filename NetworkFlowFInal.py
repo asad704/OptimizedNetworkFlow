6c19390eb3a59bbbe060c12c0d76ccacc92367b1
Muhammad Ammar Ahmed <muhammad.ammar.ahmed@ibm.com>
2018-09-01 12:03:27 +0500

    Final network code

--- NetworkFlowFInal.py
+++ NetworkFlowFInal.py
@@ -1,3 +1,4 @@
+
 print("Loading...")
 
 import requests
@@ -258,15 +259,16 @@ for i in range(len(mainflow)):
 #weather data retrieval
 weather = {}
 weather = get_predictions(weather)
-
     
-flow=nx.DiGraph() # make a directed graph (digraph)
 
-# print(res[0])
-# print(len(res))
+
+
+
+flow=nx.DiGraph() # make a directed graph (digraph)
 for i in res:
-#     print(i)
     flow.add_node(i)
+    # print(i.name)
+    # print(i)
     
 # flow.add_path(res)
 
@@ -282,44 +284,58 @@ flow.add_edge(res[9],res[10],capacity=(res[10].capacity+res[10].inflow-res[10].o
 flow.add_edge(res[10],res[11],capacity=(res[11].capacity+res[11].inflow-res[11].outflow-res[11].level))
 flow.add_edge(res[11],res[4],capacity=(res[4].capacity+res[4].inflow-res[4].outflow-res[4].level))
 
-flow_value, flow_dict = nx.maximum_flow(flow, i, res[6])
-print(type(flow_dict))
-for key, value in flow_dict.items():
-    if(flow_value>rain):
-        if((key.capacity-key.level)>rain):
-            key.capacity = key.capacity + rain
-            flow = update_flow(flow)
-    else
-        print('It will flood now! Find the area with minimum damage')
-        
-        flow_value, flow_dict = nx.maximum_flow(flow, res[0], res[6])
-# print(flow_value)
-x = nx.nodes(flow)
-# Assume it rained at Tarbela 100 cusecs
-rain = 100
-for i in  x:
-#     print(i.name)
-    if i.name == 'Tarbela Dam':
-        if (i.capacity - i.level)> rain:
-            print(i.capacity - i.level)
-            i.level = i.level + i.capacity
-            print('Level of {} increased by {}'.format(i.name,rain))
-            print('Updating networkFlow...')
-            flow = update_flow(flow)
-#             sleep(2)
-            print('Network Flow Update :)')
-            
-        else:
-            flow_value, flow_dict = nx.maximum_flow(flow, i, res[6])
-            print('TRYING TO FIND MAX FLOW PATH TO RELEASE WATER')
-            print('Max flow is {}'.format(flow_value))
-            for key, value in flow_dict.items():
-                if(flow_value>rain):
-                    if((key.capacity-key.level)>rain):
-                        key.capacity = key.capacity + rain
-                        flow = update_flow(flow)
-                else:
-                    print('It will flood now! Find the area with minimum damage')
-    
-nx.draw_spectral(flow,**options)
+# print(list(weather.keys()))
 
+
+
+
+x = nx.nodes(flow)
+for i in x:
+     # only take the existing ones
+    for dam_name,dam_value in weather.items():
+        if dam_name == i.name:
+            print(dam_name)
+            for hour,pred in dam_value.items():
+                    
+                    if hour < 25: # only 1 day data
+                        print(i.name, hour, 'Rain Predictions {}'.format(pred))
+                        if pred[0]>50:
+                            rain = pred[1]*1000
+                            #
+                            if (i.capacity - i.level)> rain:
+                                print(i.capacity - i.level)
+                                i.level = i.level + i.capacity
+                                print('Level of {} increased by {}'.format(i.name,rain))
+                                print('Updating networkFlow...')
+                                flow = update_flow(flow)
+                    #           sleep(2)
+                                print('Network Flow Update :)')
+
+                            else:
+                                flow_value, flow_dict = nx.maximum_flow(flow, i, res[6])
+                                print('TRYING TO FIND MAX FLOW PATH TO RELEASE WATER')
+                                print('Max flow is {}'.format(flow_value))
+                                for key, value in flow_dict.items():
+                                    if(flow_value>rain):
+                                        if((key.capacity-key.level)>rain):
+                                            print('increasing water level of {} by {}'.format(key.name,rain))
+                                            key.capacity = key.capacity + rain
+                                            flow = update_flow(flow)
+                                    else:
+                                        print('It will flood now! Find the area with minimum damage')
+
+G=nx.DiGraph() # make a directed graph (digraph)
+for i in res:
+    G.add_node(i.name)
+G.add_edge(res[0].name,res[1].name,capacity =(res[1].capacity+res[1].inflow-res[1].outflow-res[1].level))
+G.add_edge(res[1].name,res[2].name,capacity =(res[2].capacity+res[2].inflow-res[2].outflow-res[2].level))
+G.add_edge(res[2].name,res[3].name,capacity=(res[3].capacity+res[3].inflow-res[3].outflow-res[3].level))
+G.add_edge(res[3].name,res[4].name,capacity=(res[4].capacity+res[4].inflow-res[4].outflow-res[4].level))
+G.add_edge(res[4].name,res[5].name,capacity=(res[5].capacity+res[5].inflow-res[5].outflow-res[5].level))
+G.add_edge(res[5].name,res[6].name,capacity=(res[6].capacity+res[6].inflow-res[6].outflow-res[6].level))
+G.add_edge(res[7].name,res[8].name,capacity=(res[8].capacity+res[8].inflow-res[8].outflow-res[8].level))
+G.add_edge(res[8].name,res[9].name,capacity=(res[9].capacity+res[9].inflow-res[9].outflow-res[9].level))
+G.add_edge(res[9].name,res[10].name,capacity=(res[10].capacity+res[10].inflow-res[10].outflow-res[10].level))
+G.add_edge(res[10].name,res[11].name,capacity=(res[11].capacity+res[11].inflow-res[11].outflow-res[11].level))
+G.add_edge(res[11].name,res[4].name,capacity=(res[4].capacity+res[4].inflow-res[4].outflow-res[4].level))
+nx.draw_spectral(G,**options)
